0cfc1affdf9089646c7f00056e2f35cf4bc66272
chingiz5408 <nazarovchingiz005@gmail.com>
2024-12-21 12:45:56 +0500

    ob havo boti

---
+++ main.py
@@ -0,0 +1,162 @@
+from datetime import datetime
+import asyncio
+import logging
+import sys
+import os
+import  requests
+from aiogram import Bot, Dispatcher, html,types
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart,Command,CommandObject
+from aiogram.types import Message,KeyboardButton,ReplyKeyboardMarkup,InputFile,FSInputFile
+import local_token as lt
+keyboard=ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="Bugungi ob havo"),KeyboardButton(text="ertangi ob havo")],
+    [KeyboardButton(text="haftalik ob Havo"),KeyboardButton(text="10 kunlik")]
+], resize_keyboard=True)
+
+
+bot = Bot(token=lt.TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    # await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+    await message.answer(text="obhavo tanlashingiz mumkin",reply_markup=keyboard)
+
+time_last = None
+last_response = None
+weather_code=-1
+weather_descriptions = {
+                    0: ("Quyoshli", "Quyoshli.jpg"),
+                    1: ("Qisman bulutli", "Qisman_bulutli.jpg"),
+                    2: ("O'rtacha bulutli", "O'rtacha_bulutli.jpg"),
+                    3: ("Qalin bulutli", "Qalin_bulutli.jpg"),
+                    45: ("Tuman", "Tuman.jpg"),
+                    48: ("Tuman (sovuq yomg'irli)", "Tuman_(sovuq_yomg'irli).jpg"),
+                    51: ("Yengil yomg'ir", "Yengil_yomg'ir.jpg"),
+                    53: ("Kuchsiz yomg'ir", "Kuchsiz_yomg'ir.jpg"),
+                    55: ("Kuchli yomg'ir", "Kuchli_yomg'ir.jpg"),
+                    56: ("Yengil muzli yomg'ir", "Yengil_muzli_yomg'ir.jpg"),
+                    57: ("Muzli yomg'ir", "Muzli_yomg'ir.jpg"),
+                    61: ("Yengil yomg'ir", "Yengil_yomg'ir.jpg"),
+                    63: ("Kuchsiz yomg'ir", "Kuchsiz_yomg'ir.jpg"),
+                    65: ("Kuchli yomg'ir", "Kuchli_yomg'ir.jpg"),
+                    66: ("Yengil muzli yomg'ir", "Yengil_muzli_yomg'ir.jpg"),
+                    67: ("Muzli yomg'ir", "Muzli_yomg'ir.jpg"),
+                    71: ("Yengil qor", "Yengil_qor.jpg"),
+                    73: ("Qor", "Qor.jpg"),
+                    75: ("Kuchli qor", "Kuchli_qor.jpg"),
+                    77: ("Qor donalari", "Qor_donalari.jpg"),
+                    85: ("Yengil qorli yomg'ir", "Yengil_qorli_yomg'ir.jpg"),
+                    86: ("Qorli yomg'ir", "Qorli_yomg'ir.jpg"),
+                    80: ("Yengil yomg'ir", "Yengil_yomg'ir.jpg"),
+                    81: ("Kuchsiz yomg'ir", "Kuchsiz_yomg'ir.jpg"),
+                    82: ("Kuchli yomg'ir", "Kuchli_yomg'ir.jpg"),
+                    95: ("Kuchsiz momaqaldiroq", "Kuchsiz_momaqaldiroq.jpg"),
+                    96: ("Kuchli momaqaldiroq", "Kuchli_momaqaldiroq.jpg"),
+                    99: ("Juda kuchli momaqaldiroq", "Juda_kuchli_momaqaldiroq.jpg"),
+                }
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    global time_last,last_response,weather_code,weather_descriptions
+    if message.text=="Bugungi ob havo":
+        now=datetime.today().hour
+        url = (
+            "https://api.open-meteo.com/v1/forecast?latitude=39.6542&longitude=66.9695"
+            "&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,windspeed_10m_max,weathercode"
+            "&timezone=auto"
+        )
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            max_temp = data['daily']['temperature_2m_max'][0]
+            min_temp = data['daily']['temperature_2m_min'][0]
+            precipitation = data['daily']['precipitation_sum'][0]
+            max_wind_speed = data['daily']['windspeed_10m_max'][0]
+            weather_code = data['daily']['weathercode'][0]
+
+            discription, rasm = weather_descriptions.get(weather_code, ("Ob-havo holati noma'lum", "unknown.png"))
+            time_last = now
+            # last_response = (
+            #     f"Kunning maksimal harorati: {max_temp}°C\n"
+            #     f"Kunning minimal harorati: {min_temp}°C\n"
+            #     f"Yog'ingarchilik miqdori: {precipitation} mm\n"
+            #     f"Bugungi ob havo: {discription} \n"
+            #     f"Shamolning maksimal tezligi: {max_wind_speed} km/soat"
+            # )
+            last_response=(
+                f"🌨 Bugungi Ob-havo Ma'lumotlari\n"
+                "━━━━━━━━━━━━━━━ \n"
+                f"📆 {datetime.now().strftime('Sana: %d-%m-%Y  Vaqt: %H:%M')}\n"
+                f"🕒 Holat: {discription} yog'moqda!\n"
+                f"❄ Haroratlar:{max_temp}°C\n"
+                f"▫ Maksimal: {max_temp}°C 🌡\n"
+                f"▫ Tunda: {min_temp}°C 🥶\n"
+                f"💧 Yog'ingarchilik miqdori: {precipitation} mm\n"
+                f"🌬 Shamol tezligi: {max_wind_speed} km/soat 🌪\n"
+                "━━━━━━━━━━━━━━━\n"
+                f"⚠️ Iltimos, ehtiyot bo'ling: qor yo'llarni sirpanchiq qilishi mumkin!\n"
+                f"❓ Bugungi rejalaringiz qanday? ❄"
+            )
+
+            rasm_file = "./galireya/"+rasm
+            # if not os.path.exists(rasm_file):
+            #     await message.answer("Kiritilgan fayl yo'q yoki noto'g'ri manzil ko'rsatilgan!")
+            # else :
+            #     await message.answer("Kiritilgan fayl bor yoki yaxshi manzil ko'rsatilgan!")
+            try:
+                photo = FSInputFile(rasm_file)
+                await bot.send_photo(chat_id=message.chat.id, photo=photo, caption=last_response)
+                  # Rasmni InputFile sifatida o'zgartirish
+                # await message.answer_photo(photo, caption="Mana sizning rasmingiz!")
+            except Exception as e:
+                await message.answer(f"Xatolik yuz berdi: {e}")
+        else:
+            await message.answer("Ob-havo ma'lumotlarini olishda xatolik yuz berdi!")
+        # if time_last is None or now != time_last:
+        #     print("salom")
+        # else:
+        #     await message.answer(f"Bugun soat-{time_last} da\n {last_response}")
+
+
+# Papka yo'li
+folder_path = "./galireya"
+
+
+@dp.message()
+async def send_photos(message: types.Message):
+    # Papkadan fayllarni o'qish
+    if not os.path.exists(folder_path):
+        await message.reply("Galireya papkasi topilmadi!")
+        return
+
+    files = os.listdir(folder_path)
+    if not files:
+        await message.reply("Papkada rasm yo'q!")
+        return
+
+    for file_name in files:
+        file_path = os.path.join(folder_path, file_name)
+        # Faylni tekshirish va jo'natish
+        if os.path.isfile(file_path):
+            try:
+                with open(file_path, 'rb') as photo:
+                    await bot.send_photo(chat_id=message.chat.id, photo=InputFile(photo))
+            except Exception as e:
+                await message.reply(f"Xatolik yuz berdi: {e}")
+
+
+async def main() -> None:
+    global  bot
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler("bot.log"),#bu qism holatni bot.log filega yozib bpradi
+            logging.StreamHandler(sys.stdout)#bu qism terminalda holatni bildirib turadi
+        ]
+                        )
+    asyncio.run(main())
